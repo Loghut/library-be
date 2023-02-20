ecf6e8abdc3bccb677844046e5e2bec4a4483069
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-02-20 16:47:51 +0100

    init

---
+++ src/main/java/Hlavna.java
@@ -0,0 +1,15 @@
+
+/**
+ * Ukazka triedy v maven projekte.
+ *
+ * @author mvagac
+ *
+ */
+public class Hlavna {
+
+	public static void main(String[] args) {
+		System.out.println("hello world..");
+	}
+
+}
+
